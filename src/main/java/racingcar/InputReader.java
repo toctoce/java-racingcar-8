9768429faf3269af622d1a92198a549d4c44d637
toctoce <toctoce@naver.com>
2025-10-24 19:00:47 +0900

    refactor(InputReader): println 함수 대신 print 함수를 사용하도록 변경

--- src/main/java/racingcar/InputReader.java
+++ src/main/java/racingcar/InputReader.java
@@ -4,18 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public final class InputReader {
 
-    private static final String GET_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String GET_ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String GET_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+    private static final String GET_ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?\n";
 
     private InputReader() {}
 
     public static String readCarNames() {
-        System.out.println(GET_NAME_MESSAGE);
+        System.out.print(GET_NAME_MESSAGE);
         return read();
     }
 
     public static String readAttemptCount() {
-        System.out.println(GET_ATTEMPT_COUNT_MESSAGE);
+        System.out.print(GET_ATTEMPT_COUNT_MESSAGE);
         return read();
     }
 
