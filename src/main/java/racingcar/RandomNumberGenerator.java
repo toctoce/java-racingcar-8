b189d8acbbea973836627b73e748d8990d136e19
toctoce <toctoce@naver.com>
2025-10-24 17:27:42 +0900

    fix(RandomNumberGenerator): 객체 생성 방지, final 클래스로 변경

--- src/main/java/racingcar/RandomNumberGenerator.java
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -2,9 +2,12 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumberGenerator {
+public final class RandomNumberGenerator {
     private static final int RANGE_START = 0;
     private static final int RANGE_END = 0;
+
+    // 객체 생성 방지
+    private RandomNumberGenerator() {}
 
     public static int generate() {
         return Randoms.pickNumberInRange(RANGE_START, RANGE_END);
--- src/test/java/racingcar/RandomNumberGeneratorTest.java
+++ src/test/java/racingcar/RandomNumberGeneratorTest.java
@@ -1,13 +1,14 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
 class RandomNumberGeneratorTest {
 
     @Test
-    void 무작위로_생성된_숫자_검증() {
+    void 무작위로_생성된_숫자가_0이상_9이하이다() {
         // given
         int randomNumber  = RandomNumberGenerator.generate();
 
