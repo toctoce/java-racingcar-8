e0bebff5480cc054c0ee6dfd879ff63068f814e7
toctoce <toctoce@naver.com>
2025-10-27 18:00:55 +0900

    fix(InputReader): 클래스가 final이 아니도록 수정

--- src/main/java/racingcar/InputReader.java
+++ src/main/java/racingcar/InputReader.java
@@ -2,7 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public final class InputReader {
+public class InputReader {
 
     private static final String GET_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
     private static final String GET_ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?\n";
